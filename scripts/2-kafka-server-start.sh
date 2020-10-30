0bf2b3eed2c39e75e764e9eabd1494f4cc5d0723
Aplostar <apachauri@ee.iitr.ac.in>
2020-10-30 16:50:09 +0530

    Initial commit

---
+++ scripts/2-kafka-server-start.sh
@@ -0,0 +1,1 @@
+kafka-server-start.sh /home/aplostar/kafka_2.13-2.6.0/config/server-2.properties
---
+++ scripts/zookeeper-start.sh
@@ -0,0 +1,1 @@
+/home/aplostar/kafka_2.13-2.6.0/bin/zookeeper-server-start.sh /home/aplostar/kafka_2.13-2.6.0/config/zookeeper.properties
